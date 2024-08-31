bf6fe3a99a942e1e15dc4e9caf5e18750d4300f4
anthony-koh <nthnykh@gmail.com>
2024-08-31 18:19:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
+import google.generativeai as genai
 from flask import Flask,render_template,request
+
+api = ""
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
-           
+
+@app.route("/ai_agent", methods=["GET","POST"])
+def ai_agent():
+    return(render_template("ai_agent.html"))
+
+@app.route("/ai_agent_reply", methods=["GET","POST"])
+def ai_agent_reply():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("ai_agent_reply.html",r=r.text))
+
+@app.route("/prediction", methods=["GET","POST"])
+def prediction():
+    return(render_template("index.html"))
+
 if __name__ == "__main__":
     app.run()
